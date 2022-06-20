830c73d5dc860933af53d85147ff76401505facd
Conan <71186629+Conanpro@users.noreply.github.com>
2022-06-20 12:25:05 +0200

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.API_KEY,
+      'x-rapidapi-key': 'df23e16127msh23774b778e48962p1c4374jsnf2671f4a0ce5',
     },
   });
 
